dfae1e38f9a8ffec08cff548aef40bdcb890728f
cesarperez <cesar_dany2009@hotmail.com>
2019-02-14 10:59:00 -0700

    Primer commit 1er Parcial

---
+++ data.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+tupla=(1,2, "hola")
+
+print(tupla)
+
+tuplanueva=1,2,3,7,3
+
+print(type(tuplanueva))
+
+print(max (tuplanueva))
+
+print(tuplanueva[4])
+
+tuplanueva=1,2,"nuevo"
+
+print(tuplanueva)
